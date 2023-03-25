594660d324bd1849f3cc8fae21531f53415f7126
Tran Quoc Phi <>
2023-03-25 13:47:37 +0700

    -login with google

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,15 +2,16 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthLayout from '../../components/AuthLayout/AuthLayout';
 import { loginAction } from '../../store/actions/auth.action';
-import { AuthContext, AuthDispatchContext, OAuth2Context } from '../../store/contexts/auth.context';
+import { AuthContext, AuthDispatchContext, OAuth2Context } from '../../contexts/auth.context';
 import { useForm } from 'react-hook-form';
 import { emailValidationProperty, passwordValidationPropery } from '../../utils/validations/auth';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const handleLoginSubmit = (data) => {
-        
+
     }
 
 
@@ -30,7 +31,17 @@ const Login = () => {
                 <button>Login</button>
             </form>
             <span>Bạn chưa có tài khoản? <Link className="we-account" to={'/register'} title="">Đăng ký</Link></span>
-            <span style={{cursor: 'pointer'}} >Đăng nhập với google</span>
+            <GoogleOAuthProvider clientId="262625104810-n7svflmq4l9c4oghoaq4j7i45jd7jn3o.apps.googleusercontent.com"
+         >
+           <GoogleLogin
+             onSuccess={async (credentialResponse) => {
+             console.log(credentialResponse.credential);
+                        }}
+             onError={() => {
+               console.log("Login Failed");
+             }}
+           />
+         </GoogleOAuthProvider>
         </AuthLayout>
     )
 }
