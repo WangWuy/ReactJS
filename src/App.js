ee5391b11d94be89e479eed77f7f397e16fda5f8
Tran Quoc Phi <>
2023-03-26 23:25:21 +0700

    update redux thunk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { ROUTERS } from './router';
 import { Route } from 'react-router-dom';
 import AuthRouter from './components/AuthRouter/AuthRouter';
-import { useReducer } from 'react';
-import { AuthContext, AuthDispatchContext, OAuth2Context } from './contexts/auth.context';
-import { authReducer, initialAuth } from './store/reducers/auth.reducer';
-import { useAuth0 } from '@auth0/auth0-react';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,15 @@
-import { memo, useContext, useEffect } from "react"
+import { memo } from "react"
 import { Button } from "react-bootstrap"
-import { useNavigate } from "react-router-dom"
 import Layout from "../../components/Layout/Layout"
-import { removeLocalStorage, setLocalStorage } from "../../services/auth/localstorage.service"
 import { logoutAction } from "../../store/actions/auth.action"
-import { AuthContext, AuthDispatchContext, OAuth2Context } from "../../contexts/auth.context"
 import { useDispatch, useSelector } from "react-redux"
 
 const Home = () => {
-    const auth = useSelector(state => state.authReducer);
-    const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleLogout = (e) => {
         dispatch(logoutAction());
     }
-    
+
     return (
         <Layout showSidebar>
             <h2>Home</h2>
